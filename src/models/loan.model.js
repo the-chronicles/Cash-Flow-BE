9a57ffc7c4ae302628a033a2a4c1e13e9175b537
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2025-05-19 07:27:25 +0100

    loan model

--- src/models/loan.model.js
+++ src/models/loan.model.js
@@ -1,74 +1,89 @@
 const mongoose = require("mongoose");
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const loanSchema = new mongoose.Schema({
-  loanType: {
-    type: String,
-    required: true,
-    enum: ['personal', 'home', 'auto', 'education', 'business']
+const loanSchema = new mongoose.Schema(
+  {
+    loanType: {
+      type: String,
+      required: true,
+      enum: ["personal", "home", "auto", "education", "business"],
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 50,
+      max: 500,
+    },
+    paymentFrequency: {
+      type: String,
+      required: true,
+      enum: ["weekly", "monthly"],
+    },
+    installments: {
+      type: Number,
+      required: true,
+    },
+    purpose: {
+      type: String,
+      required: true,
+    },
+    interestRate: {
+      type: Number,
+      default: 8.9,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "approved", "rejected", "paid"],
+    },
+    employment: {
+      type: String,
+      required: true,
+      enum: [
+        "full_time",
+        "part_time",
+        "self_employed",
+        "unemployed",
+        "retired",
+      ],
+    },
+    income: {
+      type: Number,
+      required: true,
+    },
+    payFrequency: {
+      type: String,
+      required: true,
+      enum: ["weekly", "biweekly", "semimonthly", "monthly"],
+    },
+    nextPayDate: {
+      type: Date,
+      required: true,
+    },
+    hasDirectDeposit: {
+      type: Boolean,
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LoanProduct",
+      required: false,
+    },
+
+    socialSecurityNumber: String,
+    idType: String,
+    idDocumentPath: String,
+    accountType: String,
+    routingNumber: String,
+    accountNumber: String,
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-    min: 50,
-    max: 500
-  },
-  paymentFrequency: {
-    type: String,
-    required: true,
-    enum: ['weekly', 'monthly']
-  },
-  installments: {
-    type: Number,
-    required: true
-  },
-  purpose: {
-    type: String,
-    required: true
-  },
-  interestRate: {
-    type: Number,
-    default: 8.9
-  },
-  status: {
-    type: String,
-    default: 'pending',
-    enum: ['pending', 'approved', 'rejected', 'paid']
-  },
-  employment: {
-    type: String,
-    required: true,
-    enum: ['full_time', 'part_time', 'self_employed', 'unemployed', 'retired']
-  },
-  income: {
-    type: Number,
-    required: true
-  },
-  payFrequency: {
-    type: String,
-    required: true,
-    enum: ['weekly', 'biweekly', 'semimonthly', 'monthly']
-  },
-  nextPayDate: {
-    type: Date,
-    required: true
-  },
-  hasDirectDeposit: {
-    type: Boolean,
-    required: true
-  },
-  socialSecurityNumber: String,
-  idType: String,
-  idDocumentPath: String,
-  accountType: String,
-  routingNumber: String,
-  accountNumber: String,
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 loanSchema.plugin(mongoosePaginate);
 
