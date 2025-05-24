0a5bc9e3b9c132b965fd80b622b8ec90db9e4f5d
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2025-05-24 15:46:46 +0100

    loan product

--- src/utils/auth.middleware.js
+++ src/utils/auth.middleware.js
@@ -16,8 +16,8 @@ module.exports = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Attach the decoded user data to the request object
-    // req.user = decoded;
-    req.user = { _id: decoded.id }; // ✅ now req.user._id works
+    req.user = decoded;
+    // req.user = { _id: decoded.id }; // ✅ now req.user._id works
 
 
     // Proceed to the next middleware or route handler
